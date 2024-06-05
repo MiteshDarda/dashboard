0bd40cb6d3318bafde5404f10ad85da26b7c211b
mitesh darda <mitesh.darda.0@gmail.com>
2024-06-05 18:04:11 +0530

    feat: complete heading

--- src/components/dashboard/helper/Heading/index.tsx
+++ src/components/dashboard/helper/Heading/index.tsx
@@ -1,8 +1,18 @@
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import MoreVertOutlinedIcon from "@mui/icons-material/MoreVertOutlined";
+
 function Heading() {
   return (
     <div className="w-full flex justify-between items-center">
-      <div className="montserrat-500">Dashboard</div>
-      <div>b</div>
+      <div className="montserrat-500 text-[24px]">Dashboard</div>
+      <div>
+        <div className="w-[132px] h-[32px] bg-white flex justify-center items-center rounded-full">
+          <SearchOutlinedIcon className=" border-r-[1px] grow border-[#CBCBCB]" />
+          <NotificationsOutlinedIcon className=" border-r-[1px] grow border-[#CBCBCB]" />
+          <MoreVertOutlinedIcon className="grow" />
+        </div>
+      </div>
     </div>
   );
 }
