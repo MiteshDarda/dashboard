7315405e01a436914f517e2fbe85a6448ecc1c00
mitesh darda <mitesh.darda.0@gmail.com>
2024-06-06 12:26:39 +0530

    feat: create store

--- src/components/dashboard/helper/priority_alerts/helper/alert_card.tsx
+++ src/components/dashboard/helper/priority_alerts/helper/alert_card.tsx
@@ -1,5 +1,8 @@
 import { FC, ReactElement } from "react";
 import Button from "@mui/material/Button";
+import { useDispatch } from "react-redux";
+import { setMessage } from "../../../../../store/reducers/message_slice";
+import { MessageTypeEnum } from "../../../../../store/reducers/enums/message_type.enum";
 
 export interface AlertCardProps {
   avatar: ReactElement;
@@ -18,6 +21,23 @@ const AlertCard: FC<AlertCardProps> = ({
   date,
   description,
 }) => {
+  const dispatch = useDispatch();
+  const resolveHandler = () => {
+    dispatch(
+      setMessage({
+        type: MessageTypeEnum.SUCCESS,
+        text: "Resolved using Redux",
+      }),
+    );
+  };
+  const ignoreHandler = () => {
+    dispatch(
+      setMessage({
+        type: MessageTypeEnum.WARNING,
+        text: "Ignored using Redux",
+      }),
+    );
+  };
   return (
     <div className="flex flex-col justify-between items-center p-2 w-[380px] h-[170px] gap-4 bg-white rounded-lg shadow-[0_2px_8px_)_rgba(26, 56, 117, 1]">
       <div className="grow w-full flex justify-between">
@@ -40,10 +60,15 @@ const AlertCard: FC<AlertCardProps> = ({
         <Button
           variant="text"
           sx={{ textDecoration: "underline", color: "#1A3875" }}
+          onClick={ignoreHandler}
         >
           Ignore
         </Button>
-        <Button variant="contained" sx={{ backgroundColor: "#1A3875" }}>
+        <Button
+          variant="contained"
+          sx={{ backgroundColor: "#1A3875" }}
+          onClick={resolveHandler}
+        >
           Resolve
         </Button>
       </div>
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./styles/output.css";
+import { Provider } from "react-redux";
+import store from "./store/index.ts";
+import Background from "./components/backgorund/index.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  <Provider store={store}>
+    <Background />
     <App />
-  </React.StrictMode>,
+  </Provider>,
 );
